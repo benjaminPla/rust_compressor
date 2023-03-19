9c40a9f51f065d680e551a8403402cb36eebe61b
benjaminPla <benjamin.pla@7-11.com>
2023-03-19 09:08:41 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main() {
+    if args().len() != 3 {
+        eprintln!("`source` and `target` are mandatory parameters");
+        return;
+    }
+
+    let source = File::open(args().nth(1).unwrap()).unwrap();
+    let mut input = BufReader::new(source);
+
+    let target = args().nth(2).unwrap();
+    let output = File::create(target).unwrap();
+
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder).unwrap();
+
+    let output = encoder.finish().unwrap();
+
+    println!("Source len: {}", input.get_ref().metadata().unwrap().len());
+    println!("Target len: {}", output.metadata().unwrap().len());
+    println!("Elapse: {:?}", start.elapsed());
+}
